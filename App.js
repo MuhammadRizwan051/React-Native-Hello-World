c58fc1d000e120298808b389be7aad05150f16ca
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-11-23 15:36:52 +0500

    Hello World in React Native App

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function App() {
+
+  return (
+    <View>
+      <Text style={{fontSize:50, textAlign:'center', fontFamily:'cursive', backgroundColor:'black', color:'white', justifyContent:'center', alignItems:'center', height:'100' }}>Hello World</Text>
+    </View>
+  )
+}
+
+export default App  
